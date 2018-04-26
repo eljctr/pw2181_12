2d0539bdf5174fdcb280d6e041f70362d81958d0
CCDM27 <ccdm27@CCDM27.local>
2018-04-26 12:53:18 -0600

    Marvel

---
+++ marvel/js/pimerjquery.js
@@ -0,0 +1,18 @@
+var inicia=function(){
+	var nuevo=function(){
+		$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data){
+  	$("#nombre").html(data.results[0].name.first+" "+data.results[0].name.last);
+  	$("#foto").attr("src",data.results[0].picture.large);
+  }
+	});
+	}
+$("#btnNuevo").on("click",nuevo);
+
+
+var inicia = function(){
+	
+}
+$(document).ready(inicia);
