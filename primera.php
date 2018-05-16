aa6ed1efc8d07d770c8c9f8dd6f924294502e3aa
CCDM27 <ccdm27@CCDM27.local>
2018-05-16 12:56:44 -0600

    php

---
+++ primera.php
@@ -0,0 +1,28 @@
+
+
+
+<?php
+         $p1=$_GET["PARAMETRO1"]
+        for($i=0;$i<10; $i++){
+        	
+        	print("Hola PHP ". $i."<br>" );
+
+        }
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	
+	<?php print($p1); ?>
+</body>
+</html>
